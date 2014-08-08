f567ac1f8dfd7163208e1940da77e9aa9e696e83
merlyn.enriquez <merlyn.enriquez@gmail.com>
2014-08-08 04:08:56 +0000

    Problemas de Layz de la tabla Auditoria

--- dirandro-web/src/main/java/pe/gob/mininter/dirandro/util/beanbase/BeanBase.java
+++ dirandro-web/src/main/java/pe/gob/mininter/dirandro/util/beanbase/BeanBase.java
@@ -2,16 +2,8 @@ package pe.gob.mininter.dirandro.util.beanbase;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
-
 public abstract class BeanBase implements Serializable {
 
 	private static final long serialVersionUID = -106090697239179889L;
-
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
-	}
 
 }
