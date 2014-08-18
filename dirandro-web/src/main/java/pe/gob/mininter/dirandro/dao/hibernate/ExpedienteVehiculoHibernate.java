620d37cbaeb1bdf73592ea6837e123db75015b15
chrismindedness <chrismindedness@gmail.com>
2014-08-18 06:01:33 +0000

    expediente vehiculo Hibernate

---
+++ dirandro-web/src/main/java/pe/gob/mininter/dirandro/dao/hibernate/ExpedienteVehiculoHibernate.java
@@ -0,0 +1,7 @@
+package pe.gob.mininter.dirandro.dao.hibernate;
+
+import pe.gob.mininter.dirandro.model.DetPerVehExp;
+
+public interface ExpedienteVehiculoHibernate extends BaseHibernate<DetPerVehExp,Long>{
+
+}
