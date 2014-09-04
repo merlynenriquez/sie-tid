8387fca313f231c98522761cb69c064be9004815
merlyn.enriquez@gmail.com <merlyn.enriquez@gmail.com@9b313ffe-dcae-a3ba-f1e1-2a56a5b4b1b6>
2014-09-04 01:15:47 +0000

    Actualizacion de Centro Poblado

--- trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/CentroPobladoServiceImpl.java
+++ trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/CentroPobladoServiceImpl.java
@@ -2,6 +2,9 @@ package pe.gob.mininter.dirandro.service.impl;
 
 import java.util.List;
 
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +14,24 @@ import pe.gob.mininter.dirandro.service.CentroPobladoService;
 import pe.gob.mininter.dirandro.util.Busqueda;
 
 @Service
-public class CentroPobladoServiceImpl extends BaseServiceImpl<CentroPoblado, Long>
-	implements CentroPobladoService {
-
-	/**
-	 * 
-	 */
+public class CentroPobladoServiceImpl extends BaseServiceImpl<CentroPoblado, Long> implements CentroPobladoService {
+	
 	private static final long serialVersionUID = 3448706158479870676L;
+	
 	private CentroPobladoHibernate centroPobladoHibernate;
 	
+	@Override
+	public void actualizar(CentroPoblado object) {
+		object.validar();
+		super.actualizar(object);
+	}
+
+	@Override
+	public void crear(CentroPoblado object) {
+		object.validar();
+		super.crear(object);
+	}
+
 	@Autowired
 	public CentroPobladoServiceImpl(CentroPobladoHibernate centroPobladoHibernate) {
 		super(centroPobladoHibernate);
@@ -29,6 +41,23 @@ public class CentroPobladoServiceImpl extends BaseServiceImpl<CentroPoblado, Lon
 	@Override
 	public List<CentroPoblado> listarCentroPoblado() {
 		Busqueda filtro = Busqueda.forClass(CentroPoblado.class);
+		return centroPobladoHibernate.buscar(filtro);
+	}
+
+	@Override
+	public List<CentroPoblado> buscar(CentroPoblado centroPoblado) {
+		Busqueda filtro = Busqueda.forClass(CentroPoblado.class);
+		
+		if (centroPoblado!=null){
+			if (centroPoblado.getId() != null) {
+				filtro.add(Restrictions.eq("id", centroPoblado.getId()));
+			}
+			if(centroPoblado.getNombre()!= null){
+				filtro.add(Restrictions.ilike("nombre",centroPoblado.getNombre(), MatchMode.ANYWHERE));
+			}
+		}
+		
+		filtro.addOrder(Order.asc("nombre"));
 		return centroPobladoHibernate.buscar(filtro);
 	}
 
