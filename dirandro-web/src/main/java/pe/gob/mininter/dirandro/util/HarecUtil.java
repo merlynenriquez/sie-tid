be66d401a8d9c00a3a3cf16c9c98e5d930e6318e
essenwanger <essenwanger@gmail.com>
2014-10-02 03:39:22 +0000

    Componentes para Validaciones de Campos

--- dirandro-web/src/main/java/pe/gob/mininter/dirandro/util/HarecUtil.java
+++ dirandro-web/src/main/java/pe/gob/mininter/dirandro/util/HarecUtil.java
@@ -1,5 +1,7 @@
 package pe.gob.mininter.dirandro.util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -7,9 +9,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -17,6 +24,9 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.vaadin.ui.Window;
+
+import pe.gob.mininter.dirandro.exception.ValidacionException;
 import pe.gob.mininter.dirandro.model.Delito;
 import pe.gob.mininter.dirandro.model.Dependencia;
 import pe.gob.mininter.dirandro.model.Equipo;
@@ -558,5 +568,35 @@ public abstract class HarecUtil {
 			return StringUtils.EMPTY;
 		}
 	}
+	
+	public static void validar (Window window, Object object){
+        ValidatorFactory factory=Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Field[] allFields = object.getClass().getDeclaredFields();
+	    for (Field field : allFields) {
+	        if (Modifier.isPrivate(field.getModifiers())) {
+	            Set<ConstraintViolation<Object>> constraintViolations = validator.validateProperty(object, field.getName());
+	            for (ConstraintViolation<Object> constraintViolation : constraintViolations) {
+	                throw new ValidacionException(constraintViolation.getMessage(),
+	                                new Object[]{constraintViolation.getPropertyPath()});
+	                }
+	        }
+	    }
+	}
+	
+	public static void validar (Window window, Object object, BeanValidar[] listValidar){
+	        ValidatorFactory factory=Validation.buildDefaultValidatorFactory();
+	        Validator validator = factory.getValidator();
+	    for (BeanValidar validar : listValidar) {
+	    Set<ConstraintViolation<Object>> constraintViolations = validator.validateProperty(object, validar.getPropiedad());
+	    for (ConstraintViolation<Object> constraintViolation : constraintViolations) {
+	        if(validar.getObjecto()!=null){
+	                validar.getObjecto().focus();
+	        }
+	        throw new ValidacionException(constraintViolation.getMessage(),validar.getArgs());
+	        }
+	    }
+	}
+	
 }
 
