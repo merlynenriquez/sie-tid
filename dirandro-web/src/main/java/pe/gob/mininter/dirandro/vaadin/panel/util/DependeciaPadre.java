915a61c2d5087a962abde106ee10a12f641b847d
lekvispk <lekvispk@gmail.com>
2014-10-10 06:56:14 +0000

    cambio en manejo de popup para seleccion de dependencia, se ha agregado una Interface para que soporte multiples ventanas padre

---
+++ dirandro-web/src/main/java/pe/gob/mininter/dirandro/vaadin/panel/util/DependeciaPadre.java
@@ -0,0 +1,13 @@
+package pe.gob.mininter.dirandro.vaadin.panel.util;
+
+/**
+ * Interfaz que permite que distitnas ventanas invoquen sin problemas al panel de registro de documento<br> 
+ * Pensado para poder cargar el popup de seleccion de dependencias
+ * @author ElvisRubén
+ *
+ */
+public interface DependeciaPadre {
+
+	public void obtenerDependencia();
+	
+}
