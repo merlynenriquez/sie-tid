e193e6f373dc055685634d63efc0094cd83ce1ef
essenwanger@gmail.com <essenwanger@gmail.com@9b313ffe-dcae-a3ba-f1e1-2a56a5b4b1b6>
2014-10-20 06:09:07 +0000

    Busqueda Persona + Documentos

--- trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/InmuebleServiceImpl.java
+++ trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/InmuebleServiceImpl.java
@@ -38,17 +38,31 @@ public class InmuebleServiceImpl extends BaseServiceImpl<Inmueble, String> imple
 				filtro.add(Restrictions.eq("id", inmueble.getId()));
 			}
 			addILikeRestrictions(filtro, "descripcion", inmueble.getDescripcion());
-			
+			if(inmueble.getTipoDireccion()!=null){
+				filtro.createAlias("tipoDireccion", "td");
+				filtro.add(Restrictions.eq("td.id", inmueble.getTipoDireccion().getId()));
+			}
 			addILikeRestrictions(filtro, "direccion", inmueble.getDireccion());
 			addILikeRestrictions(filtro, "urbanizacion", inmueble.getUrbanizacion());
-			
+			if(inmueble.getUbgDistrito()!=null){
+				filtro.createAlias("ubgDistrito", "u");
+				filtro.add(Restrictions.eq("u.id", inmueble.getUbgDistrito().getId()));
+			}
 			addILikeRestrictions(filtro, "nroInscripcion", inmueble.getNroInscripcion());
 			addILikeRestrictions(filtro, "oficinaRegistral", inmueble.getOficinaRegistral());
 			addILikeRestrictions(filtro, "resolucion", inmueble.getResolucion());
-			
+			if(inmueble.getValorBien()!=null){
+				filtro.add(Restrictions.eq("valorBien", inmueble.getValorBien()));
+			}
+			if(inmueble.getValorTerreno()!=null){
+				filtro.add(Restrictions.eq("valorBien", inmueble.getValorTerreno()));
+			}
 			addILikeRestrictions(filtro, "observaciones", inmueble.getObservaciones());
 			addILikeRestrictions(filtro, "longitud", inmueble.getLongitud());
 			addILikeRestrictions(filtro, "latitud", inmueble.getLatitud());
+			if(inmueble.getDimension()!=null){
+				filtro.add(Restrictions.eq("dimension", inmueble.getDimension()));
+			}
 		}
 		return expedienteInmuebleHibernate.buscar(filtro);
 	}
