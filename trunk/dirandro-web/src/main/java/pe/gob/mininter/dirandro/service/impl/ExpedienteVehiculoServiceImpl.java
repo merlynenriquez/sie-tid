52240aaa625012b63b3b74eb14040eba9ef0b72b
edson.a1518@gmail.com <edson.a1518@gmail.com@9b313ffe-dcae-a3ba-f1e1-2a56a5b4b1b6>
2014-10-21 08:07:38 +0000

    busqueda de vehiculos y telefono

--- trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/ExpedienteVehiculoServiceImpl.java
+++ trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/ExpedienteVehiculoServiceImpl.java
@@ -2,6 +2,8 @@ package pe.gob.mininter.dirandro.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 import pe.gob.mininter.dirandro.dao.hibernate.ExpedienteVehiculoHibernate;
 import pe.gob.mininter.dirandro.exception.ValidacionException;
 import pe.gob.mininter.dirandro.model.DetPerVehExp;
+import pe.gob.mininter.dirandro.model.Expediente;
+import pe.gob.mininter.dirandro.model.Vehiculo;
 import pe.gob.mininter.dirandro.service.ExpedienteVehiculoService;
 import pe.gob.mininter.dirandro.util.Busqueda;
 import pe.gob.mininter.dirandro.util.Constante;
@@ -32,6 +36,47 @@ public class ExpedienteVehiculoServiceImpl extends BaseServiceImpl<DetPerVehExp,
 	@Override
 	public List<DetPerVehExp> buscar(DetPerVehExp detPerVehExp) {
 		Busqueda filtro = Busqueda.forClass(DetPerVehExp.class);
+		
+		Vehiculo vehiculo = detPerVehExp.getVehiculo();
+		if (vehiculo != null) {
+			filtro.createAlias("vehiculo", "v");
+			if (vehiculo.getPlaca()!= null) {
+				logger.debug("placa " + vehiculo.getPlaca() );
+				filtro.add(Restrictions.ilike("v.placa",vehiculo.getPlaca(), MatchMode.ANYWHERE));
+			}
+			if (vehiculo.getSerieChasis()!= null) {
+				filtro.add(Restrictions.ilike("v.serieChasis",vehiculo.getSerieChasis(), MatchMode.ANYWHERE));
+			}
+			if (vehiculo.getSerieMotor()!= null) {
+				filtro.add(Restrictions.ilike("v.serieMotor",vehiculo.getSerieMotor(), MatchMode.ANYWHERE));
+			}
+			if(vehiculo.getModeloMarca()!=null){
+				filtro.createAlias("v.modeloMarca", "mm");
+				if( !StringUtils.isEmpty( vehiculo.getModeloMarca().getNombre() )){
+					logger.debug("modelo "+vehiculo.getModeloMarca().getNombre() );
+					filtro.add(Restrictions.ilike("mm.nombre", vehiculo.getModeloMarca().getNombre() , MatchMode.ANYWHERE));	
+				}
+			}
+			if(vehiculo.getPeriodoFabricacion()!=null){
+				filtro.createAlias("v.periodoFabricacion", "pf");
+				if( !StringUtils.isEmpty( vehiculo.getPeriodoFabricacion().getNombre() )){
+					logger.debug("anio " + vehiculo.getPeriodoFabricacion().getNombre() );
+					filtro.add(Restrictions.ilike("pf.nombre", vehiculo.getPeriodoFabricacion().getNombre() , MatchMode.ANYWHERE));	
+				}
+			}
+		}
+		Expediente  expediente = detPerVehExp.getExpediente();
+		if(expediente!=null)
+		{
+			filtro.createAlias("expediente", "e");
+			
+			if (expediente.getFechaRegistro() !=null){
+				filtro.add(Restrictions.ge("e.fechaRegistro", expediente.getFechaRegistro()));
+			}
+			if (expediente.getFechaRegistroFinal() !=null){
+				filtro.add(Restrictions.le("e.fechaRegistro", expediente.getFechaRegistroFinal()));
+			}
+		}
 		return expVehiculoHibernate.buscar(filtro);
 	}
 	
