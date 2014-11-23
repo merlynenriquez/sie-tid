f03ffa115d162f2d4e3e51d73ac3ccb968296c10
lekvispk@gmail.com <lekvispk@gmail.com@9b313ffe-dcae-a3ba-f1e1-2a56a5b4b1b6>
2014-11-23 01:34:46 +0000

    Informe de inteligencia - registro de gremios cocaleros y popup

--- trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/GremioServiceImpl.java
+++ trunk/dirandro-web/src/main/java/pe/gob/mininter/dirandro/service/impl/GremioServiceImpl.java
@@ -1,11 +1,15 @@
 package pe.gob.mininter.dirandro.service.impl;
 
+import java.util.List;
+
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pe.gob.mininter.dirandro.dao.hibernate.GremioHibernate;
 import pe.gob.mininter.dirandro.model.GremioCocalero;
 import pe.gob.mininter.dirandro.service.GremioService;
+import pe.gob.mininter.dirandro.util.Busqueda;
 
 @Service
 @SuppressWarnings("serial")
@@ -17,6 +21,39 @@ public class GremioServiceImpl extends BaseServiceImpl<GremioCocalero, Long> imp
 	public GremioServiceImpl(GremioHibernate gremioHibernate) {
 		super(gremioHibernate);
 		this.gremioHibernate = gremioHibernate;
+	}
+
+	@Override
+	public List<GremioCocalero> buscar(GremioCocalero gremio) {
+		Busqueda filtro = Busqueda.forClass(GremioCocalero.class);		
+		if(gremio!=null){
+			
+			logger.debug("bean detalle lleno");
+//			if(caso.getDireccion()!=null){
+//				logger.debug(" tiene direccion ");
+//				filtro.createAlias("direccion", "d");
+//				if(caso.getDireccion().getId()!=null){
+//					logger.debug(" direccion id " + caso.getDireccion().getId() );	
+//					filtro.add(Restrictions.eq("d.id", caso.getDireccion().getId() ));			
+//				}
+//				if(!HarecUtil.nullToEmpty( caso.getDireccion().getDireccion() ).equals("")){
+//					filtro.add(Restrictions.ilike("d.direccion", caso.getDireccion().getDireccion(), MatchMode.ANYWHERE ));			
+//				}
+//			}
+//			
+//				filtro.createAlias("caso", "c");
+//			
+//				if(!HarecUtil.nullToEmpty( caso.getCaso().getNombre() ).equals("")){
+//					filtro.add(Restrictions.ilike("c.nombre", caso.getCaso().getNombre(), MatchMode.ANYWHERE ));			
+//				}
+//			
+			
+			if(gremio.getId()!=null){
+				filtro.add(Restrictions.eq("id", gremio.getId() ));			
+			}
+				
+		}
+		return gremioHibernate.buscar(filtro);
 	}
 	
 }
